1c3cb6da4d3c024fe3987d0bba2ca8c7d5545cb1
OlgaMuravha <olgamuravha@gmail.com>
2023-05-16 03:15:51 -0700

    0x09-static_libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,22 +1,13 @@
 #include "main.h"
 /**
- * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
- *
- * Return: s1[i] - s2[i]
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
  */
-int _strcmp(char *s1, char *s2)
+void _puts(char *str)
 {
-int i;
-i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
+while (*str != '\0')
 {
-if (s1[i] != s2[i])
-{
-return (s1[i] - s2[i]);
+_putchar(*str++);
 }
-i++;
-}
-return (0);
+_putchar('\n');
 }
