7bf2838f91875aee8d378e06dae37c9737761a14
OlgaMuravha <olgamuravha@gmail.com>
2023-06-10 13:07:20 -0700

    print

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,11 +8,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-if (index >= sizeof(unsigned long int) * 8)
-{
+int bit_val;
+if (index > 63)
 return (-1);
-}
-unsigned long int mask = (1UL << index);
-int bit_val = (n & mask) >> index;
+bit_val = (n >> index) & 1;
 return (bit_val);
 }
