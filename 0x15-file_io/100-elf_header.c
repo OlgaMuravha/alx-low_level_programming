f12727a1c33adb573bcd4a23e4f70cf6a73d1c61
OlgaMuravha <olgamuravha@gmail.com>
2023-06-17 04:29:45 -0700

    commit

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -19,103 +19,101 @@ void close_elf(int elf);
 
 void check_elf(unsigned char *e_ident)
 {
-    for (int index = 0; index < 4; index++)
-    {
-        if (e_ident[index] != ELFMAG[index])
-        {
-            fprintf(stderr, "Error: Not an ELF file\n");
-            exit(98);
-        }
-    }
+for (int index = 0; index < 4; index++)
+{
+if (e_ident[index] != ELFMAG[index])
+{
+fprintf(stderr, "Error: Not an ELF file\n");
+exit(98);
 }
-
+}
+}
 void print_magic(unsigned char *e_ident)
 {
-    printf(" Magic: ");
-    for (int index = 0; index < EI_NIDENT; index++)
-    {
-        printf("%02x ", e_ident[index]);
-    }
-    printf("\n");
+printf(" Magic: ");
+for (int index = 0; index < EI_NIDENT; index++)
+{
+printf("%02x ", e_ident[index]);
 }
-
+ printf("\n");
+}
 void print_class(unsigned char *e_ident)
 {
-    printf(" Class: ");
-    switch (e_ident[EI_CLASS])
-    {
-    case ELFCLASSNONE:
-        printf("none\n");
-        break;
-    case ELFCLASS32:
-        printf("ELF32\n");
-        break;
-    case ELFCLASS64:
-        printf("ELF64\n");
-        break;
-    default:
-        printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-    }
+printf(" Class: ");
+switch (e_ident[EI_CLASS])
+{
+case ELFCLASSNONE:
+printf("none\n");
+break;
+case ELFCLASS32:
+printf("ELF32\n");
+break;
+case ELFCLASS64:
+printf("ELF64\n");
+break;
+default:
+printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 }
-
+}
 void print_data(unsigned char *e_ident)
 {
-    printf(" Data: ");
-    switch (e_ident[EI_DATA])
-    {
-    case ELFDATANONE:
-        printf("none\n");
-        break;
-    case ELFDATA2LSB:
-        printf("2's complement, little endian\n");
-        break;
-    case ELFDATA2MSB:
-        printf("2's complement, big endian\n");
-        break;
-    default:
-        printf("<unknown: %x>\n", e_ident[EI_DATA]);
-    }
+printf(" Data: ");
+switch (e_ident[EI_DATA])
+{
+case ELFDATANONE:
+printf("none\n");
+break;
+case ELFDATA2LSB:
+printf("2's complement, little endian\n");
+break;
+case ELFDATA2MSB:
+printf("2's complement, big endian\n");
+break;
+default:
+printf("<unknown: %x>\n", e_ident[EI_DATA]);
 }
-
+}
 void print_version(unsigned char *e_ident)
 {
-    printf(" Version: %d", e_ident[EI_VERSION]);
-    switch (e_ident[EI_VERSION])
-    {
-    case EV_CURRENT:
-        printf(" (current)\n");
-        break;
-    default:
-        printf("\n");
-        break;
-    }
+printf(" Version: %d", e_ident[EI_VERSION]);
+switch (e_ident[EI_VERSION])
+{
+case EV_CURRENT:
+printf(" (current)\n");
+break;
+default:
+printf("\n");
+break;
 }
-
+}
 void print_osabi(unsigned char *e_ident)
 {
-    printf(" OS/ABI: ");
-    switch (e_ident[EI_OSABI])
-    {
-    case ELFOSABI_NONE:
-        printf("UNIX - System V\n");
-        break;
-    case ELFOSABI_HPUX:
-        printf("UNIX - HP-UX\n");
-        break;
-    case ELFOSABI_NETBSD:
-        printf("UNIX - NetBSD\n");
-        break;
-    case ELFOSABI_LINUX:
-        printf("UNIX - Linux\n");
-        break;
-    case ELFOSABI_SOLARIS:
-        printf("UNIX - Solaris\n");
-        break;
-    case ELFOSABI_IRIX:
-        printf("UNIX - IRIX\n");
-        break;
-    case ELFOSABI_FREEBSD:
-        printf("UNIX - FreeBSD\n");
-        break;
-    case ELFOSABI_TRU64:
-        printf("UNIX - TRU64\n");
+printf(" OS/ABI: ");
+switch (e_ident[EI_OSABI])
+{
+case ELFOSABI_NONE:
+printf("UNIX - System V\n");
+break;
+case ELFOSABI_HPUX:
+printf("UNIX - HP-UX\n");
+break;
+case ELFOSABI_NETBSD:
+printf("UNIX - NetBSD\n");
+break;
+case ELFOSABI_LINUX:
+printf("UNIX - Linux\n");
+break;
+case ELFOSABI_SOLARIS:
+printf("UNIX - Solaris\n");
+break;
+case ELFOSABI_IRIX:
+printf("UNIX - IRIX\n");
+break;
+case ELFOSABI_FREEBSD:
+printf("UNIX - FreeBSD\n");
+break;
+case ELFOSABI_TRU64:
+printf("UNIX - TRU64\n");
+free(header);
+close_elf(o);
+return (0);
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,44 +1,45 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-char *create_buffer(void);
-void close_file(FILE *file);
-
+char *create_buffer(char *file);
+void close_file(int fd);
 /**
- * create_buffer - allocates buffer
- * Return: an allcaated buffer
- * char *create_buffer(void)
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
  */
+char *create_buffer(char *file)
 {
-char *buffer = malloc(sizeof(char) * 1024);
+char *buffer;
+buffer = malloc(sizeof(char) * 1024);
 if (buffer == NULL)
 {
-fprintf(stderr, "Error: Can't allocate buffer\n");
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", file);
 exit(99);
 }
 return (buffer);
 }
-
 /**
- * close_file - Closes a file.
- * @file: The file pointer to be closed.
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
-void close_file(FILE *file)
+void close_file(int fd)
 {
-if (fclose(file) != 0)
+int c;
+c = close(fd);
+if (c == -1)
 {
-fprintf(stderr, "Error: Can't close file\n");
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
 }
-
 /**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  * Return: 0 on success.
- *
  * Description: If the argument count is incorrect - exit code 97.
  * If file_from does not exist or cannot be read - exit code 98.
  * If file_to cannot be created or written to - exit code 99.
@@ -46,44 +47,38 @@ exit(100);
  */
 int main(int argc, char *argv[])
 {
-FILE *file_from, *file_to;
+int from, to, r, w;
 char *buffer;
-size_t bytes_read, bytes_written;
 if (argc != 3)
 {
-fprintf(stderr, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-buffer = create_buffer();
-file_from = fopen(argv[1], "r");
-if (file_from == NULL)
+buffer = create_buffer(argv[2]);
+from = open(argv[1], O_RDONLY);
+r = read(from, buffer, 1024);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+do {
+if (from == -1 || r == -1)
 {
-fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
-file_to = fopen(argv[2], "w");
-if (file_to == NULL)
+w = write(to, buffer, r);
+if (to == -1 || w == -1)
 {
-fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", argv[2]);
 free(buffer);
-fclose(file_from);
 exit(99);
 }
-while ((bytes_read = fread(buffer, sizeof(char), 1024, file_from)) > 0)
-{
-bytes_written = fwrite(buffer, sizeof(char), bytes_read, file_to);
-if (bytes_written != bytes_read)
-{
-fprintf(stderr, "Error: Write to file %s failed\n", argv[2]);
+r = read(from, buffer, 1024);
+to = open(argv[2], O_WRONLY | O_APPEND);
+} while (r > 0);
 free(buffer);
-fclose(file_from);
-fclose(file_to);
-exit(99);
-}
-}
-free(buffer);
-close_file(file_from);
-close_file(file_to);
+close_file(from);
+close_file(to);
 return (0);
 }
